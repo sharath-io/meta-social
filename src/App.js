bb27a3e5e81c8af0de41bfdb1e71548ad0b3ec1b
Sharath <sharath.esr@gmail.com>
2023-06-15 12:52:32 +0530

    added backend & installed required packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Mockman from "mockman-js";
 
 function App() {
   return (
     <div className="App">
       <h1>Meta-Social</h1>
       <p>A social media app</p>
+      <Mockman/>
     </div>
   );
 }
