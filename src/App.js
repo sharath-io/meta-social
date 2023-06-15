18da530488ff1dae37848b788b206f61eb5f50e0
Sharath <sharath.esr@gmail.com>
2023-06-16 03:08:36 +0530

    style: updated styling for navbar & app

--- src/App.js
+++ src/App.js
@@ -6,8 +6,17 @@ function App() {
     <div className="App">
       <div className='row'>
         <div className="col col-one" ><Navbar/></div>
-        <div className="col col-two">User feed</div>
-        <div className="col col-three" >others</div>
+        <div className="col col-two">
+          <div className='display-content'>
+             <div className='content-heading'>User feed</div>
+          </div>
+        </div>
+        <div className="col col-three">
+          <div className='login-signup'>
+           <button className='login-btn'>Login</button>
+           <button className='signup-btn'>Join meta-social</button>
+          </div>
+        </div>
       </div>
     </div>
   );
