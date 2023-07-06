4db53de21660f688eac66da0d547ef22e1eff98c
Sharath <sharath.esr@gmail.com>
2023-07-06 18:28:37 +0530

    feat: created a basic profile card

---
+++ src/components/ProfileCard/profileCard.jsx
@@ -0,0 +1,7 @@
+export function ProfileCard({userDetails}){
+    return (
+        <div>
+            <h1>This is profile card of user {userDetails.username}</h1>
+        </div>
+    )
+}
--- src/pages/Profile/profile.jsx
+++ src/pages/Profile/profile.jsx
@@ -1,20 +1,26 @@
+import { useContext } from "react";
 import { useParams } from "react-router-dom"
+
+import { PostContext } from "../../contexts/postContext";
+import { ProfileCard } from "../../components/ProfileCard/profileCard";
 
 export function Profile(){
     const {username} = useParams();
+    const {postState} = useContext(PostContext);
+    const userDetails = postState.users.find(user => user.username===username);
+    
     return (
         <div>
 
             <div className='display-content'>
-             <div className='content-heading'>Profile page</div>
+             <div className='content-heading'>@{userDetails.username}</div>
             </div>
 
             <div className="main-content">
-               <p> this is profile {username} </p>
-
-               
+               <div>
+                <ProfileCard userDetails={userDetails} key={userDetails._id}/>
+               </div>  
             </div>
-            
         </div>
     )
 }
