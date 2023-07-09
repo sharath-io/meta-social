8231a05ea71adb005de05e4a44abada52547130d
Sharath <sharath.esr@gmail.com>
2023-07-10 04:13:23 +0530

    style: added icons & updated styling  for navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,4 +1,9 @@
 import {NavLink} from 'react-router-dom';
+import { MdOutlineExplore } from "react-icons/md";
+import { IoHomeOutline } from "react-icons/io5";
+import { MdOutlineBookmarkBorder } from "react-icons/md";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
 
 import './navbar.css';
 
@@ -7,10 +12,10 @@ export function Navbar(){
     return (
         <div className="navbar-container">
             <div className="brand-name">meta-social</div>
-            <NavLink to="/"  className="nav-item">Home</NavLink>
-            <NavLink to="/explore"  className="nav-item">Scroll</NavLink >
-            <NavLink to="/bookmark"  className="nav-item">Bookmarks</NavLink >
-            <NavLink to="/liked"  className="nav-item">Liked Posts</NavLink >
+            <NavLink to="/"  className="nav-item"><IoHomeOutline className='nav-icon'/>Home</NavLink>
+            <NavLink to="/explore"  className="nav-item"><MdOutlineExplore  className='nav-icon'/>Scroll</NavLink >
+            <NavLink to="/bookmark"  className="nav-item"><MdOutlineBookmarkBorder  className='nav-icon'/>Bookmarks</NavLink >
+            <NavLink to="/liked"  className="nav-item"><FavoriteBorderIcon  className='nav-icon'/>Liked Posts</NavLink >
             {/* <NavLink to="/" className="nav-item">Liked posts</NavLink > */}
         </div>
     )
