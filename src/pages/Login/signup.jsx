81560bd404da61b1d502e8ecef25f1b4eaef02a3
Sharath <sharath.esr@gmail.com>
2023-07-20 16:01:19 +0530

    feat: added background image for new signup user

--- src/pages/Login/signup.jsx
+++ src/pages/Login/signup.jsx
@@ -14,7 +14,8 @@ export function Signup(){
         username: '',
         password: '',
         confirmPassword:'',
-        avatar: 'https://i.pravatar.cc/150?img=11'
+        avatar: 'https://i.pravatar.cc/150?img=11',
+        backgroundImage: 'https://images.unsplash.com/photo-1490598000245-075175152d25?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
     });
 
     const signupHandler = (e) =>{
